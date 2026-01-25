326e0c870c681daeb0937d8cf3a4f1fd79aff683
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2026-01-25 23:30:02 +0530

    ..

--- frontend/src/pages/ArticleDetail.jsx
+++ frontend/src/pages/ArticleDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import API from "../api/axios";
 import { motion, useScroll, useSpring, AnimatePresence } from "framer-motion";
